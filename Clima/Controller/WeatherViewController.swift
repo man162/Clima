07713038bf951559c5540975367700c3a68c8aab
Manpreet Singh <manpreet.singh@cineplex.com>
2020-02-07 18:11:12 -0500

    adding the UiTextField events:- pressing enter; finish editing, should finish editing

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -13,12 +13,38 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
-    
+    @IBOutlet weak var searchTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
 
+}
+
+extension WeatherViewController: UITextFieldDelegate {
+
+    //this will work when user presses enter
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true) //this is for dismising the keyboard
+        print(searchTextField.text!)
+        return true
+    }
+
+    // user clicked elesewhere :- good for validation
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Please enter the text "
+            return false
+        }
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+          searchTextField.text = ""
+    }
 
 }
+
 
